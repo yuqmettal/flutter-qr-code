6572c65a256d3963208ae456a50cec4051921e70
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-22 09:30:15 -0500

    Add qr scan

---
+++ lib/src/pages/directions_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DirectionsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Directions Page'),
+    );
+  }
+}
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,81 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_app/src/pages/address_page.dart';
+import 'package:qr_app/src/pages/directions_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Home Page'),
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _buildNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scanQR,
+        child: Icon(Icons.filter_center_focus),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
     );
+  }
+
+  _buildNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones'),
+        ),
+      ],
+    );
+  }
+
+  _callPage(int currentPage) {
+    switch (currentPage) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DirectionsPage();
+      default:
+        return MapasPage();
+    }
+  }
+
+  _scanQR() async {
+    dynamic futureString = '';
+    try {
+      futureString = await BarcodeScanner.scan();
+    } catch (e) {
+    futureString = e.toString();
+    }
+    print('Future string: ${futureString.toString()}');
+
+    if (futureString != null) {
+      print('Tenemos informacion');
+    }
   }
 }
