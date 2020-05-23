c0d7a25fd1dd559e4f49761703536e1bc1e776ec
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-23 10:36:23 -0500

    Refactor

--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -5,33 +5,33 @@ class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<ScanModel>>(
-        future: DBProvider.db.getAllScans(),
-        builder:
-            (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final scans = snapshot.data;
-          if (scans.length == 0) {
-            return Center(
-              child: Text('No hay informacion'),
-            );
-          }
-          return ListView.builder(
-            itemBuilder: (context, i) => ListTile(
-              leading: Icon(
-                Icons.cloud_queue,
-                color: Theme.of(context).primaryColor,
-              ),
-              title: Text(scans[i].valor),
-              trailing: Icon(
-                Icons.keyboard_arrow_right,
-                color: Colors.grey,
-              ),
-            ),
+      future: DBProvider.db.getAllScans(),
+      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
           );
-        });
+        }
+        final scans = snapshot.data;
+        if (scans.length == 0) {
+          return Center(
+            child: Text('No hay informacion'),
+          );
+        }
+        return ListView.builder(
+          itemBuilder: (context, i) => ListTile(
+            leading: Icon(
+              Icons.cloud_queue,
+              color: Theme.of(context).primaryColor,
+            ),
+            title: Text(scans[i].valor),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
