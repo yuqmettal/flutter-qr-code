5f6fa83572540419069d475e64546d6ef3bf2572
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-23 10:34:15 -0500

    Rename address page

---
+++ lib/src/pages/maps_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:qr_app/src/providers/db_provider.dart';
+
+class MapsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<ScanModel>>(
+        future: DBProvider.db.getAllScans(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final scans = snapshot.data;
+          if (scans.length == 0) {
+            return Center(
+              child: Text('No hay informacion'),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (context, i) => ListTile(
+              leading: Icon(
+                Icons.cloud_queue,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text(scans[i].valor),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+            ),
+          );
+        });
+  }
+}
